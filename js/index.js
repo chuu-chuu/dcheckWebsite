92d9f99caebe93830cc7119485a142b92561e372
Chuu Chuu <chuumyatthwe225@gmail.com>
2022-09-16 21:14:54 +0630

    update for dCheck Website

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('header .uk-navbar-right a').click(function(){
+      $('.uk-navbar-right a').removeClass("uk-active");
+      $(this).addClass("uk-active");
+  });
+    
+});
+
+
